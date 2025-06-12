1632b29f50274f5845c177723832a4c8d0c72c8d
adcwa <541172290@qq.com>
2025-06-12 05:20:53 +0000

    support ai model config

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,14 @@ st.write(
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_base_url = st.text_input("OpenAI BASE URL")
+model_name =st.text_input("MODEL NAME")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key=openai_api_key, base_url=openai_base_url)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -41,7 +43,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model=model_name,
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
